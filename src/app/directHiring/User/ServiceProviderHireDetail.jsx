e50506c7c6ca36b4b568bdd6f17f81839df987fd
dppatel8085 <devrajpatel8085@gmail.com>
2025-08-27 18:32:36 +0530

    pages creation"

--- src/app/directHiring/User/ServiceProviderHireDetail.jsx
+++ src/app/directHiring/User/ServiceProviderHireDetail.jsx
@@ -8,8 +8,20 @@ import hisWorkImg from "../../../assets/directHiring/his-work.png";
 import { useEffect, useState } from "react";
 
 import ServiceProviderHisWork from "./ServiceProviderHisWork";
+import HireModel from "./HireModel";
+import PaymentModel from "./paymentModel";
 
 export default function ServiceProviderHireDetail() {
+  const [isHireModel, setIsHireModel] = useState(false);
+  const [isPaymentModelOpen,setIsPaymentModelOpen]=useState(true)
+
+const closeHireModel=()=>{
+  setIsHireModel(false)
+}
+const closePaymentModel=()=>{
+  setIsPaymentModelOpen(false)
+}
+
   return (
     <>
       <Header />
@@ -96,11 +108,11 @@ export default function ServiceProviderHireDetail() {
                   standard dummy text ever since the 1500s, when an unknown
                   printer took a galley of type and scrambled it to make a type
                   specimen book. It has survived not only five centuries, but
-                  also the leap into electronic typesetting.
-                   standard dummy text ever since the 1500s, when an unknown
-                  printer took a galley of type and scrambled it to make a type
-                  specimen book. It has survived not only five centuries, but
-                  also the leap into electronic typesetting.
+                  also the leap into electronic typesetting. standard dummy text
+                  ever since the 1500s, when an unknown printer took a galley of
+                  type and scrambled it to make a type specimen book. It has
+                  survived not only five centuries, but also the leap into
+                  electronic typesetting.
                 </p>
               </div>
 
@@ -128,6 +140,8 @@ export default function ServiceProviderHireDetail() {
         {<ServiceProviderHisWork />}
       </div>
       <Footer />
+      {isHireModel && <HireModel open={isHireModel} close={closeHireModel} />}
+      {isPaymentModelOpen && <PaymentModel open={isPaymentModelOpen} close={closePaymentModel} />}
     </>
   );
 }
