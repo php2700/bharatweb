0ce2776b8cb8747d145940dc917bd0a926644d9c
php junior <php2.dbvertex@gmail.com>
2025-09-08 10:27:04 +0530

    git

--- src/app/Bidding/Worker/Bid.jsx
+++ src/app/Bidding/Worker/Bid.jsx
@@ -8,16 +8,99 @@ import hisWorkImg from "../../../assets/directHiring/his-work.png";
 import { Search } from "lucide-react";
 
 import BidModel from "./BidModel";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
 
 export default function Bid() {
+  const BASE_URL = import.meta.env.VITE_API_BASE_URL;
   const { id } = useParams(); // agar route me /bid/:id hai to ye kaam karega
   const [isBidModel, setIsBidModel] = useState(false);
+   const [data, setData] = useState(null);
+   const [offer, setOffer] = useState("");
+  const [isOfferActive, setIsOfferActive] = useState(false);
   const [worker, setWorker] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  useEffect(() => {
+    const token = localStorage.getItem("bharat_token");
+    
 
+    fetch(`${BASE_URL}/negotiations/getLatestNegotiation/${id}`, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Failed to fetch data");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setData(data);
+      })
+      .catch((err) => {
+        setError(err.message);
+      });
+  }, []);
+    
+  let order_id='';
+  let service_provider_get_id='';
+  let user_get_id='';
+  let bidding_offer_id='';
+      if(data){
+        order_id=data.order_id;
+        service_provider_get_id=data.service_provider;
+        user_get_id=data.user;
+        bidding_offer_id=data._id;
+
+
+      }
+
+ const handleNagotiation=async(offer)=>{
+   // user id from localStorage
+    const token = localStorage.getItem("bharat_token"); // bearer token from localStorage
+
+    if (!offer) {
+      toast.error("Please enter offer amount ❗");
+      return;
+    }
+
+    try {
+      const response = await fetch(`${BASE_URL}/negotiations/start`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`, // bearer token
+        },
+        body: JSON.stringify({
+          order_id:order_id, // from location.state
+          bidding_offer_id:bidding_offer_id, // from location.state
+          service_provider: service_provider_get_id, // from URL
+          user: user_get_id, // from localStorage
+          initiator: "service_provider", // fixed
+          offer_amount: Number(offer), // from parameter
+          message: `Can you do it for ${offer}?`, // dynamic with offer
+        }),
+      });
+
+      const data = await response.json();
+      console.log("Negotiation API Response:", data);
+
+      if (response.ok) {
+        toast.success(`You sent ₹${offer} Amount For Negotiation`);
+      } else {
+        alert(`Error: ${data.message || "Something went wrong"}`);
+      }
+    } catch (error) {
+      console.error("API Error:", error);
+      alert("Failed to start negotiation ❌");
+    }
+  }
   const closeBidModel = () => {
     setIsBidModel(false);
   };
@@ -74,15 +157,15 @@ export default function Bid() {
 
     fetchWorkDetails();
   }, [id]);
-
+  
   if (loading) return <div className="text-center py-6">Loading...</div>;
   if (error)
     return <div className="text-center py-6 text-red-500">{error}</div>;
-console.log(worker);
+;
   return (
     <>
       <Header />
-      
+      <ToastContainer position="top-right" autoClose={3000} />
       <div className="min-h-screen p-4 sm:p-6 bg-gray-50">
         <div className="container max-w-5xl mx-auto my-10 p-8 shadow-lg rounded-3xl">
           <div className="text-2xl text-center font-bold mb-4">Work Detail</div>
@@ -136,7 +219,59 @@ console.log(worker);
               </div>
             </div>
           )}
+               <div className="flex flex-col items-center  p-6 ">
+            <div className="flex space-x-4 mb-12 bg-[#EDEDED] rounded-[50px] p-[12px]">
+              <button
+                onClick={() => setIsOfferActive(true)}
+                className={`px-16 py-2 rounded-full font-medium shadow-sm ${
+                  isOfferActive
+                    ? "bg-[#228B22] text-white border border-green-600"
+                    : "border border-green-600 text-green-600"
+                }`}
+              >
+                Offer Price ({data.offer_amount})
+              </button>
+              <button
+                onClick={() => setIsOfferActive(false)}
+                className={`px-16 py-2 rounded-full font-medium shadow-md ${
+                  !isOfferActive
+                    ? "bg-[#228B22] text-white hover:bg-[#228B22]"
+                    : "border border-green-600 text-green-600"
+                }`}
+              >
+                Negotiate
+              </button>
+            </div>
+
+            {!isOfferActive && (
+  <input
+    type="number"
+    placeholder="Enter your offer amount"
+    value={offer}
+    onChange={(e) => setOffer(e.target.value)}
+    className="w-[531px] px-4 py-2 border-2 border-[#dce1dc] rounded-md text-center text-[#453e3f] placeholder-green-600 focus:outline-none focus:ring-2 focus:ring-[#d1d1d1]"
+  />
+)}
+
+</div>
+
+{/* Accept / Send Request */}
+<div className="text-center">
+  <button
+    className="bg-[#228B22] text-white w-100 px-10 py-3 rounded-md font-semibold"
+    onClick={() => {
+      if (isOfferActive) {
+        alert("Request Accepted ✅");
+      } else {
+        handleNagotiation(offer);
+      }
+    }}
+  >
+    {isOfferActive ? "Accept Request" : "Send Request"}
+  </button>
+</div>
         </div>
+       
       </div>
       <Footer />
       {isBidModel && <BidModel isOpen={isBidModel} onClose={closeBidModel} orderId={id}/>}
