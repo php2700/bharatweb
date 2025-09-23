7b4ec54cf8f86a3dd2916b55b31d4d82e8ee4c49
DB Vertex <=>
2025-09-23 18:28:05 +0530

    dist

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    // ✅ Explicitly disable lightningcss and use PostCSS + esbuild
+    transformer: 'postcss',
+    minify: 'esbuild'
+  },
 })
